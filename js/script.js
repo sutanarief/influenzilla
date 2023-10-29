69f5c78ded482af5bee2770cbe9dad26befcde01
sutanarief <sutan1412@gmail.com>
2023-10-29 14:14:37 +0700

    finish section 2

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 const menuToggle = document.getElementById('menuToggle')
-const section = document.getElementById('sectionContainer01')
+const section1 = document.getElementById('sectionContainer01')
+const section2 = document.getElementById('sectionContainer02')
 const body = document.getElementsByTagName('BODY')[0]
 const menuList = document.getElementById('menuList')
-document.addEventListener('click', () => {
+menuToggle.addEventListener('click', () => {
   if(menuToggle.textContent === 'MENU') {
     body.style.background = '#111'
     menuToggle.textContent = 'Close'
     menuList.style.display = 'flex'
+    section1.style.display = 'none'
+    section2.style.display = 'none'
   } else {
     body.style.background = "url('../images/bg_section_mobile_01.png'), lightgray 50% / cover no-repeat"
     menuToggle.textContent = 'MENU'
     menuList.style.display = 'none'
+    section1.style.display = 'flex'
+    section2.style.display = 'flex'
   }
 })
