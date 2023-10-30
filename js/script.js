6ea3b28106fefb05a88a8413919d8952cbb418f8
sutanarief <sutan1412@gmail.com>
2023-10-31 00:52:47 +0700

    finish mobile display

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-const menuToggle = document.getElementById('menuToggle')
-const section1 = document.getElementById('sectionContainer01')
-const section2 = document.getElementById('sectionContainer02')
-const body = document.getElementsByTagName('BODY')[0]
-const menuList = document.getElementById('menuList')
-menuToggle.addEventListener('click', () => {
-  if(menuToggle.textContent.includes('MENU')) {
-    body.style.background = '#111'
-    menuToggle.textContent = 'Close'
-    menuList.style.display = 'flex'
-    section1.style.display = 'none'
-    section2.style.display = 'none'
-  } else {
-    body.style.background = "url('../images/bg_section_mobile_01.png'), lightgray 50% / cover no-repeat"
-    menuToggle.textContent = 'MENU'
-    menuList.style.display = 'none'
-    section1.style.display = 'flex'
-    section2.style.display = 'flex'
-  }
-})
+$(document).ready(function () {
+  
+});
